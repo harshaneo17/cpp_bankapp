b1c9bfa638f284a5369af265b5bfc03ac26d84d3
harshaneo17 <harsha.god86@gmail.com>
2022-12-19 21:29:12 +0000

    first commit with GUI function

---
+++ banking_app.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+void GuiScreen(){
+    std::cout << "                --------------------                " << std::endl;
+    std::cout << "                |                  |                " << std::endl;
+    std::cout << "                |                  |                " << std::endl;
+    std::cout << "                |      WELCOME     |                " << std::endl;
+    std::cout << "                |                  |                " << std::endl;
+    std::cout << "                |                  |                " << std::endl;
+    std::cout << "                --------------------                " << std::endl;
+}
+
+
+int main(){
+    GuiScreen();
+}
