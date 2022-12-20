43d76df57372d2369dc865af1a4a4b1921a0ff58
harshaneo17 <harsha.god86@gmail.com>
2022-12-20 06:52:49 +0000

    added options to the menu

--- banking_app.cpp
+++ banking_app.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+int user_input;
+
 void GuiScreen(){
     std::cout << "                --------------------                " << std::endl;
     std::cout << "                |                  |                " << std::endl;
@@ -8,6 +10,14 @@ void GuiScreen(){
     std::cout << "                |                  |                " << std::endl;
     std::cout << "                |                  |                " << std::endl;
     std::cout << "                --------------------                " << std::endl;
+
+    std::cout << "         CHOOSE AN OPTION AND ENTER THE NUMBER      " << std::endl;
+    std::cout << "         1-->CREATE A NEW ACCOUNT         " << std::endl;
+    std::cout << "         2-->WITHDRAW MONEY               " << std::endl;
+    std::cout << "         3-->DISPLAY BALANCE        " << std::endl;
+    std::cout << "         4-->DEPOSIT MONEY         " << std::endl;
+    std::cout << "         5-->EDIT ACCOUNT INFORMATION         " << std::endl;
+    std::cin >> user_input;
 }
 
 
