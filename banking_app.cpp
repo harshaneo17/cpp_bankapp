cb2362ef85bdc24c178e8915c1cc2a387b49114f
harshaneo17 <harsha.god86@gmail.com>
2022-12-20 20:43:57 +0000

    added switch case and function

--- banking_app.cpp
+++ banking_app.cpp
@@ -17,10 +17,60 @@ void GuiScreen(){
     std::cout << "         3-->DISPLAY BALANCE        " << std::endl;
     std::cout << "         4-->DEPOSIT MONEY         " << std::endl;
     std::cout << "         5-->EDIT ACCOUNT INFORMATION         " << std::endl;
+    std::cout << "         6-->QUIT                   " << std::endl;
     std::cin >> user_input;
 }
 
+void CreateNewAccount(){
+    std::string name;
+    std::string account_number;
+    std::string password;
+    std::string user_decision;
 
+    std::cout << "         NEW ACCOUNT         " << std::endl;
+    std::cout << "         ENTER YOUR NAME     \n" << "->>" << std::endl;
+    std::cin >> name;
+    std::cout << "         ACCOUNT NUMBER     \n" << "->>" << std::endl;
+    std::cin >> account_number;
+    std::cout << "         ENTER PASSWORD      \n" << "->>" << std::endl;
+    std::cin >> password;
+    std::cout << "Please confirm your details and confirm by typing y to proceed or n to edit\n" << name << "\n" << account_number << "\n" << password << "\n" << std::endl;
+    std::cin >> user_decision;
+    if (user_decision == "y"){
+         std::cout << "thank you for creating a new account" << std::endl;
+    }
+    else if(user_decision == "n"){
+        CreateNewAccount();
+    }
+    else{
+         std::cout << "please enter y or n";
+    } 
+}
+
+void UserInputSwitch(){
+    switch (user_input)
+    {
+    case 1:
+        CreateNewAccount();
+        break;
+    case 2:
+        //insert code
+        break;
+    case 3:
+        //insert code
+        break;
+    case 4:
+        //insert code
+        break;
+    case 5:
+        //insert code
+        break;
+    default:
+        std::cout << "         THANK YOU FOR USING OUR APPLICATION. HAVE A NICE DAY         " <<std::endl;
+        break;
+    }
+}
 int main(){
     GuiScreen();
+    UserInputSwitch();
 }
