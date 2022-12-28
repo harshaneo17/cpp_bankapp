60fba5e6cd34a14039436a18dafde4f9fcf9cd9a
harshaneo17 <harsha.god86@gmail.com>
2022-12-28 22:27:26 +0000

    new game app cpp

---
+++ game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+
+
+void Board(){
+     
+     std::cout << "ssssssssssssssssssssssssssssss" << std::endl;
+     for(int i=0;i<9;i++){
+        std::cout << "|                            |" << std::endl;
+     }
+     std::cout << "ssssssssssssssssssssssssssssss" << std::endl; 
+     std::cout << "|                            |" << std::endl;
+     std::cout << "|  PRESS   Q     TO    EXIT  |" << std::endl;
+     std::cout << "|                            |" << std::endl;
+     std::cout << "ssssssssssssssssssssssssssssss" << std::endl;
+
+}
+
+
+int main(){
+    while(true)
+      system("cls");
+      Board();
+}
